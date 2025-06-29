71ce22dafa8233e0c880635eb8b0a0f7cc7fc38b
HLMC <hl2278535805@outlook.com>
2025-06-29 20:24:28 +0800

    update service

--- action.sh
+++ action.sh
@@ -1,5 +1,5 @@
-echo "- 音量+ 禁用面积防误触 (手写笔失效)"
-echo "- 音量- 启用面积防误触 (手写笔正常)"
+echo "- 音量+ 禁用手写笔 禁用面积防误触"
+echo "- 音量- 启用手写笔 启用面积防误触"
 
 while [ "$key_click" = "" ]; do
 	key_click=$(getevent -qlc 1 | awk '{ print $3 }' | grep 'KEY_')
@@ -9,10 +9,10 @@ done
 case $key_click in
 KEY_VOLUMEUP)
     touchHidlTest -c wo 0 32 0
-	echo "- 已禁用面积防误触"
+	echo "- 已禁用手写笔"
 	;;
 *)
     touchHidlTest -c wo 0 32 1
-	echo "- 已启用面积防误触"
+	echo "- 已启用手写笔"
 	;;
 esac
--- service.sh
+++ service.sh
@@ -1,7 +1,8 @@
 while true; do
-    touchHidlTest -c wo 0 24 1 # 平滑等级 smooth_lv (1-5)
+    touchHidlTest -c wo 0 24 0 # 平滑等级 smooth_lv (1-5)
+    echo "0" > /proc/touchpanel/smooth_level # Force set, try to disable smooth 还是禁用不掉
+
     touchHidlTest -c wo 0 25 5 # 灵敏等级 sensitive_lv (0-5)
-    # touchHidlTest -c wo 0 32 0 # 面积防误触 (0/1) 该项影响手写笔，已分离至 action.sh
-    touchHidlTest -c wo 0 38 1000
+    # touchHidlTest -c wo 0 32 0 # 是否启用手写笔 可禁用面积防误触 (0/1) 该项影响手写笔，已分离至 action.sh
     sleep 5
 done
