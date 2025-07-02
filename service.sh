31c9c8ea1b8cfa737f7314795c834614790da5c9
HLMC <hl2278535805@outlook.com>
2025-07-02 17:47:48 +0800

    update service

--- service.sh
+++ service.sh
@@ -3,6 +3,6 @@ while true; do
     echo "0" > /proc/touchpanel/smooth_level # Force set, try to disable smooth 还是禁用不掉
 
     touchHidlTest -c wo 0 25 5 # 灵敏等级 sensitive_lv (0-5)
-    # touchHidlTest -c wo 0 32 0 # 是否启用手写笔 可禁用面积防误触 (0/1) 该项影响手写笔，已分离至 action.sh
+    # touchHidlTest -c wo 0 32 0 # 是否启用手写笔 可禁用面积防误触 (0/1) 打开游戏之前记得断开手写笔连接就行了
     sleep 5
 done
