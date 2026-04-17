9c370376d741b468ecb13d91d8448bf302a487f3
HLMC <hl2278535805@outlook.com>
2026-04-17 11:46:41 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -4,12 +4,11 @@ print_modname() {
 	ui_print "*********************************"
 	ui_print "-     一加平板 2 Pro 音频优化"
 	ui_print "*********************************"
-	ui_print "- 已解锁采样率  优化触控"
-	ui_print "*********************************"
 }
 
 set_permissions() {
 	set_perm_recursive $MODPATH 0 0 0755 0644
+	set_perm_recursive $MODPATH/anymount/odm/etc 0 0 0755 0644
 	set_perm_recursive $MODPATH/system/system_ext/etc 0 0 0755 0644
 	set_perm_recursive $MODPATH/system/vendor/etc 0 0 0755 0644 u:object_r:vendor_configs_file:s0
 }
