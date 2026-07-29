ba562fb92facafd96b9dbe489570754edc2f0a2a
HLMC <hl2278535805@outlook.com>
2026-07-29 19:46:13 +0800

    chore: module info

--- customize.sh
+++ customize.sh
@@ -1,11 +1,5 @@
 SKIPUNZIP=0
 
-print_modname() {
-	ui_print "*********************************"
-	ui_print "-     一加平板 2 Pro 音频优化"
-	ui_print "*********************************"
-}
-
 set_permissions() {
 	set_perm_recursive $MODPATH 0 0 0755 0644
 	set_perm_recursive $MODPATH/anymount/odm/etc 0 0 0755 0644
@@ -13,5 +7,4 @@ set_permissions() {
 	set_perm_recursive $MODPATH/system/vendor/etc 0 0 0755 0644 u:object_r:vendor_configs_file:s0
 }
 
-print_modname
 set_permissions
